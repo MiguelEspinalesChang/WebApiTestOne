e065bdd491cd8c810e8f704f60c3af326d8725ab
Miguel_EspinalesEs <mee@topfloormarketing.net>
2023-02-13 18:33:07 -0600

    created crud

--- WebApiTestOne/Controllers/AutoresController.cs
+++ WebApiTestOne/Controllers/AutoresController.cs
@@ -1,4 +1,5 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 using WebApiTestOne.Entidades;
 
 namespace WebApiTestOne.Controllers
@@ -7,15 +8,62 @@ namespace WebApiTestOne.Controllers
     [Route("api/autores")]
     public class AutoresController : ControllerBase
     {
-        [HttpGet]
-        public ActionResult<List<Autor>> Get()
+        private readonly ApplicationDbContext context;
+
+        // contructor de la clase 
+        public AutoresController(ApplicationDbContext context)
         {
-            return new List<Autor>()
+            this.context = context;
+        }
+
+        [HttpGet]
+        public async Task<ActionResult<List<Autor>>> Get()
+        {
+           return await context.Autores.ToListAsync();  
+        }
+
+        [HttpPost]
+        public async Task<ActionResult> Post(Autor autorData)
+        {
+            context.Add(autorData);
+            await context.SaveChangesAsync();
+            return Ok();    
+        }
+
+        [HttpPut("{id:int}")]  // api/autores/*
+        public async Task<ActionResult> Put(Autor autorData, int id)
+        {
+            // validar si el id corresponde al usuario
+            if(autorData.Id != id)
             {
-                new Autor() { Id = 1, Nombre = "Juan"},
-                new Autor() { Id = 2, Nombre = "Jose"},
-                new Autor() { Id = 3, Nombre = "Manuel"}
-            };
+                return BadRequest("El Id del autor no coicide");
+            }
+
+            var existeAutor = await context.Autores.AnyAsync(x => x.Id == id);
+
+            if (!existeAutor)
+            {
+                return NotFound();
+            }
+
+            context.Update(autorData);  
+            await context.SaveChangesAsync();   
+            return Ok();    
+        }
+
+        [HttpDelete("{id:int}")] // api/autores/2
+        public async Task<ActionResult> Delete(int id)
+        {
+            var existeAutor = await context.Autores.AnyAsync(x => x.Id == id);
+
+            if (!existeAutor)
+            {
+                return NotFound();
+            }
+
+            context.Remove(new Autor() { Id = id });
+            await context.SaveChangesAsync();
+            return NoContent();
         }
     }
 }
